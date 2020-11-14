08fad9476ae5e2125010ce2eeabc8f23976739a5
Walter Smuts <smuts.walter@gmail.com>
2020-11-14 15:19:22 +0200

    Calculate RMS of wav file
    Using the hound library, this commit allows reading of a wav file. An
    example of doing processing on the data, getting the RMS value of the
    signal, is shown.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+use hound;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+    let mut reader = hound::WavReader::open(filename).unwrap();
+    let sqr_sum = reader.samples::<i16>()
+                        .fold(0.0, |sqr_sum, s| {
+        let sample = s.unwrap() as f64;
+        sqr_sum + sample * sample
+    });
+    println!("RMS is {}", (sqr_sum / reader.len() as f64).sqrt());
 }
