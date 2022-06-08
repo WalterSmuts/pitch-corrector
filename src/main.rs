1621d7e820bb4aaebffeaf12e82da7e1254fcca6
Walter Smuts <smuts.walter@gmail.com>
2022-06-08 12:19:43 +0200

    Improve rendering

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
 use cpal::{InputCallbackInfo, OutputCallbackInfo};
 use realfft::RealFftPlanner;
+use splines::{Interpolation, Key, Spline};
 
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use cpal::BufferSize;
@@ -167,43 +168,55 @@ where
 }
 
 fn draw_waveform(data: &[f32]) {
-    let mut index = -(BUFFER_SIZE as i32) / 2;
-    let mut points = Vec::with_capacity(BUFFER_SIZE);
-    for point in data {
-        points.push((index as f32, *point));
-        if points.len() == BUFFER_SIZE {
-            break;
-        }
-        index += 1;
-    }
-    Chart::new_with_y_range(
-        200,
-        100,
-        -(BUFFER_SIZE as f32) / 2.0,
-        BUFFER_SIZE as f32 / 2.0,
-        -1.0,
-        1.0,
-    )
-    .lineplot(&Shape::Points(&points))
-    .display();
+    println!("Waveform:");
+    let (width, height) = get_textplots_window_size();
+    let vec = data
+        .iter()
+        .enumerate()
+        .map(|(index, sample)| Key::new(index as f32, *sample as f32, Interpolation::Linear))
+        .collect();
+
+    let spline = Spline::from_vec(vec);
+    Chart::new_with_y_range(width, height, 0.0, BUFFER_SIZE as f32, -1.0, 1.0)
+        .lineplot(&Shape::Continuous(Box::new(|x| {
+            spline.sample(x).unwrap_or(0.0)
+        })))
+        .display();
 }
 
 fn draw_psd(data: &[f32]) {
+    println!("Power Spectral Density:");
+    let (width, height) = get_textplots_window_size();
+
     let mut real_planner = RealFftPlanner::<f32>::new();
     let r2c = real_planner.plan_fft_forward(BUFFER_SIZE);
     let mut ff_data = [0.0; BUFFER_SIZE].to_vec();
     ff_data.copy_from_slice(&data[0..BUFFER_SIZE]);
     let mut spectrum = r2c.make_output_vec();
     r2c.process(&mut ff_data, &mut spectrum).unwrap();
-    let spectrum: Vec<(f32, f32)> = spectrum
+
+    let vec: Vec<_> = spectrum
         .into_iter()
         .map(|complex| complex.norm_sqr())
         .enumerate()
-        .map(|(index, val)| ((index as f32).log2() * 102.4, val))
+        .map(|(index, val)| ((index as f32).log2() * 115.4, val))
+        .map(|(index, sample)| Key::new(index as f32, sample as f32, Interpolation::Cosine))
         .collect();
-    Chart::new_with_y_range(200, 100, 0.0, BUFFER_SIZE as f32, 0.0, 30.0)
-        .lineplot(&Shape::Points(&spectrum))
+
+    let spline = Spline::from_vec(vec);
+
+    Chart::new_with_y_range(width, height, 0.0, BUFFER_SIZE as f32, 0.0, 50.0)
+        .lineplot(&Shape::Continuous(Box::new(|x| {
+            spline.sample(x).unwrap_or(0.0)
+        })))
         .display();
+}
+
+fn get_textplots_window_size() -> (u32, u32) {
+    let (mut width, height) = termion::terminal_size().unwrap();
+    width = width * 2 - 11;
+    let height = height as f32 * 1.6;
+    (width as u32, height as u32)
 }
 
 fn draw_data(data: &[f32]) {
@@ -213,7 +226,16 @@ fn draw_data(data: &[f32]) {
 }
 
 fn main() {
-    print!("{}[2J", 27 as char);
+    ctrlc::set_handler(move || {
+        print!("{}", ansi_escapes::CursorTo::AbsoluteX(0));
+        print!("{}", ansi_escapes::ClearScreen);
+        print!("{}", termion::cursor::Show);
+        std::process::exit(130);
+    })
+    .expect("Error setting Ctrl-C handler");
+
+    print!("{}", ansi_escapes::ClearScreen);
+    print!("{}", termion::cursor::Hide);
     let opts: Opts = Opts::parse();
 
     match opts.subcmd {
