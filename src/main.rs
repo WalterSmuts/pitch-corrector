ef5a23b8af5c9a906b083196448c56e99a627f41
Walter Smuts <smuts.walter@gmail.com>
2022-06-14 11:13:49 +0200

    Remove non-realtime components

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
 use clap::Parser;
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-use cpal::{BufferSize, Stream};
 use cpal::Sample;
+use cpal::{BufferSize, Stream};
 use cpal::{InputCallbackInfo, OutputCallbackInfo};
 use cpal::{SampleRate, StreamConfig};
 use crossbeam_queue::SegQueue;
 use realfft::RealToComplex;
 use realfft::{ComplexToReal, RealFftPlanner};
 use splines::{Interpolation, Key, Spline};
-use std::f32::consts::TAU;
 use std::sync::atomic::AtomicUsize;
 use std::sync::atomic::Ordering;
+use std::sync::Arc;
 use std::sync::Mutex;
-use std::sync::{Arc, Barrier};
 use textplots::Shape;
 use textplots::{Chart, Plot};
 
@@ -23,87 +22,14 @@ const SAMPLE_RATE: u32 = 44100;
 struct Opts {
     #[clap(subcommand)]
     subcmd: SubCommand,
-    /// Sets the name of the input/output wav file
-    #[clap(short, long, default_value = "example.wav")]
-    filename: String,
 }
 
 #[derive(Parser)]
 enum SubCommand {
-    /// Generate a wav file
-    Write,
-    /// Analize a wav file
-    Read,
-    /// Play wav file and showing live levels
-    Play,
-    /// Record from microphone while showing live levels
-    Record,
-    /// Record from microphone and play directly to speakers
     Passthrough,
     /// Passthrough microphone to speakers but halve the pitch
     SimplePitchHalver,
 }
-
-fn read(filename: &String) {
-    let mut reader = hound::WavReader::open(filename).unwrap();
-    let sqr_sum = reader.samples::<i16>().fold(0.0, |sqr_sum, s| {
-        let sample = s.unwrap() as f64;
-        sqr_sum + sample * sample
-    });
-    println!("RMS is {}", (sqr_sum / reader.len() as f64).sqrt());
-}
-
-fn write(filename: &String) {
-    let spec = hound::WavSpec {
-        channels: 1,
-        sample_rate: SAMPLE_RATE,
-        bits_per_sample: 16,
-        sample_format: hound::SampleFormat::Int,
-    };
-    let mut writer = hound::WavWriter::create(filename, spec).unwrap();
-    for t in (0..SAMPLE_RATE * 5).map(|x| x as f32 / SAMPLE_RATE as f32) {
-        let sample = (TAU * t * 440.0).sin();
-        let amplitude = i16::MAX as f32;
-        writer.write_sample((sample * amplitude) as i16).unwrap();
-    }
-    writer.finalize().unwrap();
-}
-
-fn play(filename: &String) {
-    let mut reader = hound::WavReader::open(filename).unwrap();
-    let barrier = Arc::new(Barrier::new(2));
-    let barrier_clone = barrier.clone();
-    let once = std::sync::Once::new();
-
-    let stream = get_output_stream(move |data: &mut [f32], _| {
-        for sample in data.iter_mut() {
-            *sample = Sample::from(
-                &reader
-                    .samples::<i16>()
-                    .map(|sample| sample.unwrap())
-                    .next()
-                    .unwrap_or_else(|| {
-                        once.call_once(|| {
-                            barrier_clone.wait();
-                        });
-                        0
-                    }),
-            );
-        }
-        draw_data(data);
-    });
-    stream.play().unwrap();
-    barrier.wait();
-}
-
-fn record() {
-    let stream = get_input_stream(move |data: &[f32], _| {
-        draw_data(data);
-    });
-    stream.play().unwrap();
-    std::thread::park();
-}
-
 fn passthrough() {
     let _streams = setup_passthrough_processor(DisplayProcessor::new());
     std::thread::park();
@@ -345,10 +271,6 @@ fn main() {
     print!("{}", termion::cursor::Hide);
 
     match opts.subcmd {
-        SubCommand::Write => write(&opts.filename),
-        SubCommand::Read => read(&opts.filename),
-        SubCommand::Play => play(&opts.filename),
-        SubCommand::Record => record(),
         SubCommand::Passthrough => passthrough(),
         SubCommand::SimplePitchHalver => simple_pitch_halver(),
     }
