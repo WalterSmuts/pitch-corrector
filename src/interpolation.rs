6ba74b2993b1fd4a0c6a5821765e91635f618d09
Walter Smuts <smuts.walter@gmail.com>
2022-06-20 15:59:26 +0200

    Implement WhittakerShannon interpolation
    This does suffer from performance issues and "just" seems to work when I
    compile using the "release" flag. Further optimization might be possible
    using frequency domain algorithms

--- src/interpolation.rs
+++ src/interpolation.rs
@@ -23,7 +23,7 @@ impl Interpolate for [f32] {
         match method {
             InterpolationMethod::Linear => LinearInterpolate::interpolate_sample(self, index),
             InterpolationMethod::WhittakerShannon => {
-                todo!()
+                WhittakerShannonInterpolate::interpolate_sample(self, index)
             }
         }
     }
@@ -51,13 +51,35 @@ impl LinearInterpolate for [f32] {
     }
 }
 
+// TODO: Investigate fft optimization
+impl WhittakerShannonInterpolate for [f32] {
+    fn interpolate_sample(&self, j: f32) -> f32 {
+        let mut sum = 0.0;
+        for (i, x) in self.iter().enumerate() {
+            sum += x * sinc(j - i as f32);
+        }
+        sum
+    }
+}
+
+fn sinc(x: f32) -> f32 {
+    use std::f32::consts::PI;
+    if x == 0.0 {
+        1.0
+    } else {
+        (x * PI).sin() / (x * PI)
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::Interpolate;
     use super::InterpolationMethod;
+    use std::f32::consts::TAU;
 
     const BUFFER_SIZE: usize = 1024;
     const TEST_SAMPLE_SIZE: usize = BUFFER_SIZE * 10;
+    const TEST_EQUALITY_EPISLON: f32 = 0.002;
 
     #[test]
     fn linear_interpolate() {
@@ -76,6 +98,28 @@ mod test {
             assert_eq!(
                 signal(x),
                 buffer.interpolate_sample(x, InterpolationMethod::Linear)
+            )
+        }
+    }
+
+    #[test]
+    fn whittaker_shannon_interpolate() {
+        // Define band-limited signal
+        let signal = |x: f32| (TAU * x / BUFFER_SIZE as f32).sin();
+
+        // Sample initial buffer
+        let mut buffer = [0.0; BUFFER_SIZE];
+        for (x, y) in buffer.iter_mut().enumerate() {
+            *y = signal(x as f32);
+        }
+
+        // Interpolate at random samples and assert signal is equal to interpolation
+        for _ in 0..TEST_SAMPLE_SIZE {
+            let x = rand::random::<f32>() * BUFFER_SIZE as f32;
+            approx::assert_abs_diff_eq!(
+                signal(x),
+                buffer.interpolate_sample(x, InterpolationMethod::WhittakerShannon),
+                epsilon = TEST_EQUALITY_EPISLON
             )
         }
     }
